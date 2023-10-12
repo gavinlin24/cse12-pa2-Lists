3b7ed8e8f5d0d0334fa48606d0eeb4ff4565c3fc
gavinlin24 <gavinlin24@gmail.com>
2023-10-12 11:51:37 -0700

    first commit

--- pa2-starter/src/ArrayGL.java
+++ pa2-starter/src/ArrayGL.java
@@ -9,4 +9,28 @@ public class ArrayGL<E> implements MyList<E> {
     }
 
     // Fill in all required methods here
+    @Override
+    public E[] toArray(){
+        return elements;
+    }
+
+    @Override
+    public void transformAll(MyTransformer mt){
+        for(int i = 0; i < elements.length; i++){
+            elements[i] = mt.transformElement(elements[i]);
+        }
+    }
+    @Override
+    public void chooseAll(MyChooser mc){
+
+    }
+    @Override
+    public boolean isEmpty(){
+        if (this.size == 0){
+            return true;
+        }
+
+        return false;
+
+    }
 }
--- pa2-starter/src/LinkedGL.java
+++ pa2-starter/src/LinkedGL.java
@@ -1,6 +1,6 @@
 public class LinkedGL<E> implements MyList<E> {
 
-    class Node {
+    class Node{
         E value;
         Node next;
 
@@ -15,8 +15,59 @@ public class LinkedGL<E> implements MyList<E> {
 
     public LinkedGL(E[] contents) {
         // Fill in this constructor
+        if(contents.length == 0){
+            return;
+        } else {
+
+            this.front = new Node(null, null);
+            Node temp = this.front;
+
+            for (int i = 0; i < contents.length; i++){
+                temp.next = new Node(contents[i], null);
+                temp = temp.next;
+                size++;
+            }
+        }
+
     }
 
     // Fill in all methods
+
+    public E[] toArray() {
+        E[] arr = (E[]) new Object[this.size];
+        Node temp = this.front;
+
+        for(int i = 0; i < this.size; i++) {
+            arr[i] = temp.value;
+            temp = temp.next;
+        }
+    }
+
+    public void transformAll(MyTransformer mt){
+        if (this.size == 0){
+            return;
+        }
+
+        Node temp = this.front;
+
+        while (temp.next != null) {
+            E newValue = mt.transformElement(temp.value)
+            temp.value = newValue;
+            temp = temp.next;
+        }
+
+    }
+
+    public void chooseAll(MyChooser mc){
+
+    }
+
+    public boolean isEmpty(){
+        if (this.size == 0) {
+            return true;
+        }
+
+        return false;
+    }
 
 }
